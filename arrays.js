67ff36a92eaf8e2528edf1837cefa39a33974792
Ahliyah Azeez <ahliyahazeez@gmail.com.com>
2024-10-23 16:27:05 +0000

    new projects again!

--- arrays.js
+++ arrays.js
@@ -1,7 +1,34 @@
 const todo=[];
  function addTodo(){
    const inputE= document.querySelector('.name')
-   const namee=inputElement.value
+   const namee=inputE.value
    todo.push(namee);
    console.log(todo)
+   inputE.value=""
+fixing()
+
+ } 
+ const nums=[1,3,5,6,7,8]
+ let total=0
+
+ for(let i=0;i<nums.length;i++){
+ const det=nums[i]
+ total+=det
  }
+ console.log(total)
+function fixing(){
+ let todolist=''
+    for(let i=0;i<todo.length;i++){
+      const currentTodo=todo[i]
+      const html=`<p>${currentTodo}</p>`
+      todolist+=html
+      console.log(todolist)
+    document.querySelector('.button').innerHTML=todolist
+  
+    }
+  
+  }
+  function removeTodo(index) {
+    todo.splice(index, 1);
+    fixing(); 
+}
