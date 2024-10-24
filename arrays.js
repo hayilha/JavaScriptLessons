29e79fae6e3a6111378db6e071ce63b79ad39e2b
Ahliyah Azeez <ahliyahazeez@gmail.com.com>
2024-10-24 17:02:00 +0000

    more projects!

--- arrays.js
+++ arrays.js
@@ -1,10 +1,13 @@
-const todo=[];
+const todo=[{}]
  function addTodo(){
-   const inputE= document.querySelector('.name')
-   const namee=inputE.value
-   todo.push(namee);
+  const inputE= document.querySelector('.name')
+  const namee=inputE.value
+  const dateE= document.querySelector('.datei')
+  const namp=dateE.value
+   todo.push({name:namee,date:namp});
    console.log(todo)
    inputE.value=""
+   dateE.value =""
 fixing()
 
  } 
@@ -17,18 +20,18 @@ fixing()
  }
  console.log(total)
 function fixing(){
- let todolist=''
+ let todolist=""
+ 
     for(let i=0;i<todo.length;i++){
       const currentTodo=todo[i]
-      const html=`<p>${currentTodo}</p>`
+      const namie=currentTodo.name
+      const datee=currentTodo.date
+      let html = `<p>${namie} ${datee} <button onclick="todo.splice(${i}, 1); fixing();">delete</button></p>`;
       todolist+=html
-      console.log(todolist)
-    document.querySelector('.button').innerHTML=todolist
-  
+      
     }
-  
+    console.log(todolist)
+    document.querySelector('.button').innerHTML=todolist
+    
   }
-  function removeTodo(index) {
-    todo.splice(index, 1);
-    fixing(); 
-}
+ 
